b94824a777c7c61f0de8deefe2c0bc67cbbbd6c9
Tesar Rahmat Maulana <tesarrm58@gmail.com>
2024-10-30 22:32:13 +0700

    authseeder pengaturan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -60,7 +60,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AuthSeeder::class,
         ]);
-        // \App\Models\Pengaturan::factory()->count(1)->create();
+        \App\Models\Pengaturan::factory()->count(1)->create();
         // \App\Models\Guru::factory()->count(10)->create();
         // \App\Models\BidangKeahlian::factory()->count(3)->create();
         // \App\Models\Jurusan::factory()->count(5)->create();
